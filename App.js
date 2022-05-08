3559eea11b918b800d1af23b9dea2ec4c58fbd9f
kimthao.huynhthi <kimthao.huynhthi@m.citynow.vn>
2022-05-09 00:02:31 +0700

    message

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 const App = () => (
   <View>
-    <Text>Open up App ddd start n your app!</Text>
+    <Text>Open up App d start n your app!</Text>
   </View>
 );
 
